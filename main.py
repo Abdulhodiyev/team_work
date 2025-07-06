a350b3dd3a0eb376bc6cc36b0fc28ed71fe359be
Abdulhodiyev <armiyatv05@gmail.com>
2025-07-06 16:09:20 +0500

    library is created

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from calculator import add_book, show_all_books, delete_book
+
 def main():
-    print("First team work")
+    print("""
+    1. Add book
+    2. Show all books
+    3. Delete book
+    4. Exit
+    """)
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        add_book()
+    elif choice == "2":
+        show_all_books()
+    elif choice == "3":
+        delete_book()
+    else:
+        print("Bye!")
+        return
+
+    return main()
 
 
 if __name__ == "__main__":
